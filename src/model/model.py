08688885d4ea108c73451341dce8e5fe2d3099a5
Echo-Ji <bh1506jjh@163.com>
2020-04-06 16:18:58 +0800

    :hammer: SUIR to SEUIR

--- src/model/model.py
+++ src/model/model.py
@@ -19,6 +19,28 @@ def SEIISR_base_eqs(INPUT, t, beta, sigma, epsilon, _lambda_, gamma):
 
     return Y
 
+def SEIISR_v_base_eqs_v0(INPUT, t, beta, sigma, pho, epsilon, _lambda_, gamma):
+    Y = np.zeros((5))
+    beta = max(beta, 0) #* get_tr(t)
+    sigma = max(sigma, 0) * get_tr(t) 
+    pho = max(pho, 0)
+    epsilon = max(epsilon, 0)
+    _lambda_ = max(_lambda_, 0)
+    _lambda_ = min(_lambda_, 1)
+    gamma = max(gamma, 0)
+    S = INPUT[0]
+    E = INPUT[1] # U
+    I = INPUT[2]
+    IS = INPUT[3]
+    R = INPUT[4]
+    Y[0] = - (beta * S * I) - (sigma * S * max((E - (pho * IS)), 0.005*E))
+    Y[1] = (beta * S * I) + (sigma * S * max((E - (pho * IS)), 0.005*E)) - (epsilon * E)
+    Y[2] = ((1 - _lambda_) * (epsilon * E)) - (gamma * I)
+    Y[3] = (_lambda_ * (epsilon * E)) - (gamma * IS)
+    Y[4] = (gamma * (I + IS))
+
+    return Y
+
 def get_tr(t):
     # print(t)
     # tr = [4.2826, 5.2757, 5.334 , 4.3842, 4.1317, 5.2662, 5.2815, 5.3214,
@@ -46,28 +68,31 @@ def get_tr(t):
     #    0.6107, 0.6198, 0.6085, 0.613 , 0.6184, 0.6157, 0.654 , 0.6523,
     #    0.6699, 0.6743, 0.677 , 0.6507, 0.65  , 0.70]
     if(t < len(tr)):
-        return np.log1p(tr[int(t)])
+        return tr[int(t)]
     else:
         return 1.0
 
 def SEIISR_v_base_eqs(INPUT, t, beta, sigma, pho, epsilon, _lambda_, gamma):
     Y = np.zeros((5))
     beta = max(beta, 0) #* get_tr(t)
-    sigma = max(sigma, 0) * get_tr(t) 
+    sigma = max(sigma, 0) #* get_tr(t) 
     pho = max(pho, 0)
+    alpha = 0.6
     epsilon = max(epsilon, 0)
     _lambda_ = max(_lambda_, 0)
     _lambda_ = min(_lambda_, 1)
     gamma = max(gamma, 0)
     S = INPUT[0]
-    E = INPUT[1] # U
-    I = INPUT[2]
-    IS = INPUT[3]
-    R = INPUT[4]
-    Y[0] = - (beta * S * I) - (sigma * S * max((E - (pho * IS)), 0.005*E))
-    Y[1] = (beta * S * I) + (sigma * S * max((E - (pho * IS)), 0.005*E)) - (epsilon * E)
-    Y[2] = ((1 - _lambda_) * (epsilon * E)) - (gamma * I)
-    Y[3] = (_lambda_ * (epsilon * E)) - (gamma * IS)
-    Y[4] = (gamma * (I + IS))
+    E = INPUT[1]
+    U = INPUT[2]
+    I = INPUT[3]
+    IS = INPUT[4]
+    R = INPUT[5]
+    Y[0] = - (beta * S * I) - (sigma * S * max((U - (pho * IS)), 0.005*U))
+    Y[1] = (beta * S * I) + (sigma * S * max((U - (pho * IS)), 0.005*U)) - (np.exp(alpha * get_tr(t)) * E)
+    Y[2] = (np.exp(alpha * get_tr(t)) * E) - (epsilon * U)
+    Y[3] = ((1 - _lambda_) * (epsilon * U)) - (gamma * I)
+    Y[4] = (_lambda_ * (epsilon * U)) - (gamma * IS)
+    Y[5] = (gamma * (I + IS))
 
     return Y
